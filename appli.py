c63b6a4c2d33a99b9a4d163618145425872de6eb
Alexjmt <60393143+Alexjmt@users.noreply.github.com>
2020-03-31 15:51:15 +0200

    Tableau
    Liste de liste du tableau

--- appli.py
+++ appli.py
@@ -1,5 +1,5 @@
-﻿classement = open('Classement_ligue _1_(sans_#1).csv','r')
-place0 = 0
-for place in classement:
-    place0 += 1
-    print("N°",place0,":",place,end="")
+﻿classement = open('Classement_ligue _1_(avant_COVID-19).csv','r')
+equipe = classement.readlines()
+len(equipe)
+tableau = [equipe[1],equipe[2],equipe[3],equipe[4],equipe[5],equipe[6],equipe[7],equipe[8],equipe[9],equipe[10],equipe[11],equipe[12],equipe[13],equipe[14],equipe[15],equipe[16],equipe[17],equipe[18],equipe[19],equipe[20]]
+print(tableau[5])
