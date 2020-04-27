7a603a2a9fdac33eb2cf91ba016bfcda22b9aa12
BastienB17 <60393020+BastienB17@users.noreply.github.com>
2020-04-27 15:27:52 +0200

    Amélioration Afficheur Bastien
    Ajouts des différentes fenêtres, du .grid ainsi que un peu de design qui va être amélioré par la suite et création des deux listes

--- Afficheur.py
+++ Afficheur.py
@@ -10,9 +10,19 @@ window.minsize(720, 480)
 window.iconbitmap("Icon.ico")
 
 
+
 # ajouter du texte
 label_title = Label(window, text="Bienvenue sur notre application de pronostique sur la ligue 1", font=("Bauhaus 93", 35))
 label_title.pack(expand=YES)
+
+#Ajout design
+
+photo = PhotoImage(file="ligue1.png")
+
+canvas = Canvas(window,width=1080, height=720)
+canvas.create_image(0, 0, anchor=NW, image=photo)
+canvas.pack(expand=YES)
+
 
 
 def Ouvrirligue1():
@@ -24,27 +34,28 @@ def Ouvrirligue1():
 
     ligue1.title("Pronostique")
     ligue1.geometry("1080x720")
+    ligue1.config(background='blue')
     ligue1.minsize(720, 480)
 
     # Ajout du texte Equipe 1
 
     label_title = Label(ligue1, text="Equipe 1", font=("Bauhaus 93", 14))
-    label_title.pack(side=LEFT)
+    label_title.grid(row=2, column=1)
 
     # Ajout du texte Equipe 2
 
     label_title = Label(ligue1, text="Equipe 2", font=("Bauhaus 93", 14))
-    label_title.pack(side=RIGHT)
+    label_title.grid(row=2, column=4)
 
     # Ajout du texte Contre
 
     label_title = Label(ligue1, text="Contre", font=("Bauhaus 93", 14))
-    label_title.pack(expand=YES)
+    label_title.grid(row=1, column=3)
 
     #Ajout liste deroulante
 
     labelChoix = Label(ligue1, text="Veuillez choisir")
-    labelChoix.pack()
+    labelChoix.grid(row=3, column=1)
 
     #Création de la liste des équipes
 
@@ -53,7 +64,58 @@ def Ouvrirligue1():
     #Création de l'objet
 
     ListeCombo = ttk.Combobox(ligue1, values=listequipes)
-    ListeCombo.pack()
+    ListeCombo.grid(row=3, column=1)
+
+    #Création de l'objet 2
+
+    ListeCombo2 = ttk.Combobox(ligue1, values=listequipes)
+    ListeCombo2.grid(row=3, column=4)
+
+
+def OuvrirContact():
+    # Création de la fenetre Contact
+
+    Contact = Tk()
+
+    # Personnalisation de la fenetre Contact
+
+    Contact.title("Contact")
+    Contact.geometry("1080x720")
+    Contact.config(background='white')
+    Contact.minsize(720, 480)
+
+    #Ajout du texte
+
+    label_title = Label(Contact, text="Voici comment nous contacter à l'aide de notre adresse mail: Atanbatlex@pronostic.pro", font=("Bauhaus 93", 35))
+    label_title.pack(expand=YES)
+
+
+def OuvrirApropos():
+    # Création de la fenetre Apropos
+
+    Apropos = Tk()
+
+    # Personnalisation de la fenetre Apropos
+
+    Apropos.title("A-propos")
+    Apropos.geometry("1080x720")
+    Apropos.config(background='white')
+    Apropos.minsize(720, 480)
+
+
+def OuvrirAvis():
+    # Création de la fenetre Avis
+
+    Avis = Tk()
+
+    # Personnalisation de la fenetre Avis
+
+    Avis.title("Avis")
+    Avis.geometry("1080x720")
+    Avis.config(background='white')
+    Avis.minsize(720, 480)
+
+
 # Barre menu
 MenuBar = Menu(window)
 
@@ -69,8 +131,12 @@ MenuBar.add_cascade(label="Contact", menu=menuContact)
 MenuBar.add_cascade(label="A-propos", menu=menuApropos)
 MenuBar.add_cascade(label="Avis", menu=menuAvis)
 
-# Ajout de commande aux menus
+# Ajout de commandes aux menus
 menuPronostique.add_command(label="Ligue 1", command=Ouvrirligue1)
+menuContact.add_command(label="Contact", command=OuvrirContact)
+menuApropos.add_command(label="A propos", command=OuvrirApropos)
+menuAvis.add_command(label="Avis", command=OuvrirAvis)
+
 
 # Afficher la fênetre
 window.config(menu=MenuBar)
